631da7466c5b67a38a8fb3658ccad0371fcffd8b
Master Ayush Gautam <ayushgautam2007.kg@gmail.com>
2026-05-06 22:45:46 +0530

    add: create app.jsx file with initial imports for routing and components

---
+++ Frontend/src/app.jsx
@@ -0,0 +1,5 @@
+import './App.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import StoryLoader from "./components/StoryLoader"
+import StoryGenerator from "./components/StoryGenerator.jsx";
+
